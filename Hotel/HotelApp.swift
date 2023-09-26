3304ca994a3144ce5a4f9c65beeadf29ecde5290
NightBurgerus <103958372+NightBurgerus@users.noreply.github.com>
2023-09-26 12:55:11 +0300

    Initial commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Чебупелина on 26.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
